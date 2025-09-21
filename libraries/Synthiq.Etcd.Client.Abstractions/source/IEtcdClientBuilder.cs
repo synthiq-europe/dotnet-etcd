abdfa17b8d53ca26f6cd4b555f817a9706e4eb82
Synthiq Developer 2 <git@synthiq.eu>
2025-09-21 18:51:16 +0200

    refactor: collapse abstraction layers

--- libraries/Synthiq.Etcd.Client.Abstractions/source/IEtcdClientBuilder.cs
+++
@@ -1,8 +0,0 @@
-namespace Synthiq.Etcd.Client;
-
-public interface IEtcdClientBuilder
-{
-    public IEtcdClient Build();
-
-    public IEtcdClientBuilder WithTransports(IEtcdTransportConfiguration configuration);
-}
